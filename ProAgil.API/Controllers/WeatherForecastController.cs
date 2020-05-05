62f69321fa5ac1e5dc5b692eb5f768f83430dc0a
Paulo Barbosa <paulohjbarbosa@gmail.com>
2020-05-05 13:37:22 -0300

    Seção 2: Asp.NET Core - API

---
+++ ProAgil.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.API.Data;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Controllers {
+    [ApiController]
+    [Route ("[controller]")]
+    public class WeatherForecastController : ControllerBase {
+        public DataContext _context { get; }
+
+        public WeatherForecastController (DataContext context) {
+            this._context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get () {
+            try {
+                return Ok (await _context.Eventos.ToListAsync ());
+            } catch (System.Exception) {
+                return this.StatusCode (StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Evento>> Get (int id) {
+
+            return await _context.Eventos.FirstOrDefaultAsync (x => x.EventoId == id);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# ProAgil
+
+###
+
+![paulo barbosa](face-notebook_300x292.jpg)
