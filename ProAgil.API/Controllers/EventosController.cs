55ce0f4d7f05110801198a27d5f4963560f4ed0d
Paulo Barbosa <paulohjbarbosa@gmail.com>
2020-05-24 16:55:10 -0300

    Seção 7: Angular - Reactive Forms

--- ProAgil.API/Controllers/EventosController.cs
+++ ProAgil.API/Controllers/EventosController.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProAgil.Domain.Entities;
+using ProAgil.Repository.Context;
 using ProAgil.Repository.IRepositories;
 
 namespace ProAgil.API.Controllers
@@ -11,10 +12,12 @@ namespace ProAgil.API.Controllers
     public class EventosController : ControllerBase
     {
         private readonly IProAgilRepository _repo;
+        private readonly ProAgilContext _context;
 
-        public EventosController(IProAgilRepository repo)
+        public EventosController(IProAgilRepository repo, ProAgilContext context)
         {
             _repo = repo;
+            _context = context;
         }
 
         [HttpGet]
@@ -22,7 +25,8 @@ namespace ProAgil.API.Controllers
         {
             try
             {
-                return Ok(await _repo.GetAllEventosAsync(true));
+                var eventos = await _repo.GetAllEventosAsync(true);
+                return Ok(eventos);
             }
             catch (System.Exception)
             {
@@ -31,7 +35,7 @@ namespace ProAgil.API.Controllers
 
         }
 
-        [HttpGet("{Eventoid}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<Evento>> Get(int id)
         {
             try
@@ -81,19 +85,25 @@ namespace ProAgil.API.Controllers
 
         }
 
-        [HttpPut]
+        [HttpPut("{EventoId}")]
         public async Task<ActionResult<Evento>> Put(int EventoId, Evento evento)
         {
             try
             {
+
                 var _evento = await _repo.GetEventosAsyncById(EventoId, false);
                 if (_evento == null)
                 {
                     return NotFound();
                 }
+
+                // _context.Update(evento);
+                // _context.SaveChanges();
+
                 _repo.Update(evento);
                 if (await _repo.SaveChangesAsync())
                 {
+                    //return Ok(await _repo.GetAllEventosAsync(true));
                     return Created($"/api/evento/{evento.Id}", evento);
                 }
 
@@ -107,8 +117,8 @@ namespace ProAgil.API.Controllers
 
         }
 
-        [HttpDelete]
-        public async Task<ActionResult<Evento>> Put(int EventoId)
+          [HttpDelete("{EventoId}")]
+        public async Task<ActionResult<Evento>> Delete(int EventoId)
         {
             try
             {
