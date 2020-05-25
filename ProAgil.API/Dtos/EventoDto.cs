b2c658ed89a3e93fc20c994dd4d3875e9fdb0c9c
Paulo Barbosa <paulohjbarbosa@gmail.com>
2020-05-25 19:20:11 -0300

    Seção 8: AutoMapper & Data Annotations

--- ProAgil.API/Dtos/EventoDto.cs
+++ ProAgil.API/Dtos/EventoDto.cs
@@ -1,19 +1,29 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProAgil.API.Dtos
 {
     public class EventoDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [StringLength(100,MinimumLength=3, ErrorMessage="Obrigatório entre 3 e 100 caracteres")]
         public string Local { get; set; }
         public string DataEvento { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string Tema { get; set; }
+
+        [Range(2, 120000, ErrorMessage = "Quantidade de pessoas deve estar entre 2 e 120.000")]
         public int QtdPessoas { get; set; }
         public string ImagemUrl { get; set; }
         public string Telefone { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
         public List<LoteDto> Lotes { get; set; }
         public List<RedeSocialDto> RedesSociais { get; set; }
-         public List<PalestranteDto> Palestrantes { get; set; }
+        public List<PalestranteDto> Palestrantes { get; set; }
     }
 }
